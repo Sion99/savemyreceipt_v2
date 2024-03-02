a6289bc6a2ba412d7cf3ae2528d256353cc98cc6
Sion Shin <kmicety1@gmail.com>
2024-03-02 21:51:20 +0900

    Feat: CORS Setting

--- src/main/java/com/smr/savemyreceipt_v2/config/SecurityConfig.java
+++ src/main/java/com/smr/savemyreceipt_v2/config/SecurityConfig.java
@@ -9,9 +9,9 @@ import com.smr.savemyreceipt_v2.jwt.TokenProvider;
 import com.smr.savemyreceipt_v2.oauth2.OAuth2LoginFailureHandler;
 import com.smr.savemyreceipt_v2.oauth2.OAuth2LoginSuccessHandler;
 import com.smr.savemyreceipt_v2.oauth2.OAuth2UserService;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,11 +21,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -65,9 +66,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable)
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
             .headers(c -> c.frameOptions(FrameOptionsConfig::disable).disable())
-
             .oauth2Login(oauth2Login -> oauth2Login
                 .successHandler(oAuth2LoginSuccessHandler) // OAuth2 로그인 성공 핸들러
                 .failureHandler(oAuth2LoginFailureHandler) // OAuth2 로그인 실패 핸들러
@@ -83,6 +83,20 @@ public class SecurityConfig {
             }).sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // 모든 출처 허용
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH")); // 허용할 HTTP 메소드
+        configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 쿠키를 포함한 요청 허용
+        configuration.setMaxAge(3600L); // pre-flight 요청의 결과를 캐시하는 시간(초 단위)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 CORS 설정 적용
+        return source;
     }
 
 }
